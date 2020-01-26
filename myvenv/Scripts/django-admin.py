fb82f90261296228012379e9598aec03c08b76d3
jojojojo1322 <bobob935@gmail.com>
2020-01-26 18:05:35 +0900

    Reac-Django connect

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\조현재\desktop\rtc\react_django_commerce\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
